9245879e08387479cb0b2a70e28e17f0bc01ca7a
techy-geek <anuragsharma.nits@gmail.com>
2025-12-12 22:51:49 +0530

    New changes heatmap.

--- LeetStalker/src/config.js
+++ LeetStalker/src/config.js
@@ -1,3 +1,3 @@
 // This is your backend URL. 
 // If you are developing locally, you can switch this to "http://localhost:3000"
-export const API_URL = "https://leettracker-oyfh.onrender.com";
+export const API_URL = "http://leettracker-oyfh.onrender.com";

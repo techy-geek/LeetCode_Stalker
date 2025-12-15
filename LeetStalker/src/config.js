eac1530c2aa73c8af8f2e240ee4fe9394928fce8
techy-geek <anuragsharma.nits@gmail.com>
2025-12-15 09:43:02 +0530

    backend url updated

--- LeetStalker/src/config.js
+++ LeetStalker/src/config.js
@@ -1,4 +1,4 @@
 // This is your backend URL. 
 // If you are developing locally, you can switch this to "http://localhost:3000"
 // "http://leettracker-oyfh.onrender.com";
-export const API_URL = "http://leettracker-oyfh.onrender.com";
+export const API_URL = "https://leettracker-oyfh.onrender.com";
